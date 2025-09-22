336c9195ae6928a06e44f10beeabb29d3f5a530e
vivekkumar5555 <143058399+vivekkumar5555@users.noreply.github.com>
2025-09-22 17:38:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 // Configure CORS
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "https://realtimeticktockappclient.onrender.com",
+    origin:"https://realtimeticktockappclient.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -61,7 +61,7 @@ app.use(limiter);
 // CORS
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: "https://realtimeticktockappclient.onrender.com/login",
     credentials: true,
   })
 );
